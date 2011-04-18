da3553dbfb5bc43bbe5c87e1f00926bcb4e3af55
leafduo <leafduo@gmail.com>
2011-04-18 14:27:59 +0000

    Can post only 1 day's tweets

--- twitter2blogger.py
+++ twitter2blogger.py
@@ -18,26 +18,30 @@ from config import *
 twitter = OAuthApi(consumer_key, consumer_secret)
 
 # Get the temporary credentials for our next few calls
-temp_credentials = twitter.getRequestToken()
+#temp_credentials = twitter.getRequestToken()
 
 # User pastes this into their browser to bring back a pin number
-print(twitter.getAuthorizationURL(temp_credentials))
+#print(twitter.getAuthorizationURL(temp_credentials))
 
 # Do a test API call using our new credentials
-twitter = OAuthApi(consumer_key, consumer_secret, access_token['oauth_token'], access_token['oauth_token_secret'])
-user_timeline = twitter.GetUserTimeline({"trim_user": True, "since_id": since_id, "count": 200})
+#twitter = OAuthApi(consumer_key, consumer_secret, access_token['oauth_token'], access_token['oauth_token_secret'])
+
+user_timeline = twitter.GetUserTimeline({"trim_user": True, "screen_name": screen_name, "count": 200})
 
 tweets_list = []
 
-since_id = user_timeline[0]['id']
-print since_id
+now = datetime.datetime.utcnow()
+d = datetime.timedelta(days=1)
+since_time = now - d
 
 for tweet in user_timeline:
-    tweets_list.append(tweet['created_at'])
-    tweets_list.append('\n')
-    tweets_list.append(tweet['text'])
-    tweets_list.append('\n\n')
-
+    time = datetime.datetime.strptime(tweet['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
+    if time > since_time:
+        tweets_list.append(tweet['created_at'])
+        tweets_list.append('\n')
+        tweets_list.append(tweet['text'])
+        tweets_list.append('\n\n')
+    
 content = u''.join(tweets_list)
 
 smtp = smtplib.SMTP("localhost")
