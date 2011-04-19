f5ece4309e8d783f6e506db76592390dbc932601
leafduo <leafduo@gmail.com>
2011-04-19 04:20:16 +0000

    Add a sample config

---
+++ config.sample.py
@@ -0,0 +1,10 @@
+#Find them at https://dev.twitter.com/apps/
+consumer_key = ''
+consumer_secret = ''
+
+#receiver is your Blogger's email posting address
+receiver = r''
+sender = r'example@example.com'
+
+#Your screen name at twitter
+screen_name = ''
